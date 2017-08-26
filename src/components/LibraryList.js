f4838bc2a233187c05700ea04bd436adfd68c34f
Chotipat Poowongthanarat <topz.classic@gmail.com>
2017-08-26 14:28:02 +0700

    Fixbux and Styling list

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,13 +1,14 @@
 //import liraries
 import React, { Component } from 'react';
-import { View, Text, ListView, StyleSheet } from 'react-native';
+import { Text, ActivityIndicator, ListView, StyleSheet } from 'react-native';
 import { connect } from 'react-redux'
 import ListItem from './ListItem'
 
 // create a component
 class LibraryList extends Component {
-    
-    componentDidMount(){
+
+    constructor(libraries){
+        super(libraries)
         const ds = new ListView.DataSource({
             rowHasChanged:(r1,r2) => r1 !== r2
         })
@@ -16,7 +17,9 @@ class LibraryList extends Component {
     renderRow(library){
         return <ListItem library={library}/>
     }
+  
     render() {
+        console.log(this.props)
         return(
             <ListView
                 dataSource={this.dataSource}
@@ -27,9 +30,7 @@ class LibraryList extends Component {
 }
 
 const mapStateToProps = state => {
-    return {
-        libraries: state.libraries
-    }
+    return { libraries: state.libraries} 
 }
 
 export default connect(mapStateToProps)(LibraryList);
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -6,12 +6,20 @@ import {CardSection} from './common'
 // create a component
 class ListItem extends Component {
     render() {
+        const {titleStyle} = styles
         return (
             <CardSection>
-                <Text>{this.props.library.title}</Text>
+                <Text style={titleStyle}>{this.props.library.title}</Text>
             </CardSection>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    titleStyle:{
+        fontSize:18,
+        paddingLeft: 15,
+    },
+});
 //make this component available to the app
 export default ListItem;
